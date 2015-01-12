ecc7b3ebeca775dbdc597cb4620808cee1cb3945
Lukas Pokorny <lukas.pokorny@rebex.net>
2015-01-12 17:11:08 +0100

    Added actual code

---
+++ README.md
@@ -0,0 +1,12 @@
+﻿# mdns-proxy
+
+A very simple *Multicast DNS to DNS proxy*. Resolves *A* and *AAAA* **mDNS** queries using the **DNS** subsystem.
+
+Makes `mydomain.local` names work with OS X Yosemite (10.10) and iOS 8.2 (or perhaps 8.1 or 8.0 - not tested yet) again (see https://discussions.apple.com/thread/6555023?start=75&tstart=0).
+
+While this proxy certainly works, it's still far from perfect.
+
+Possible enhancements:
+- pay attention to the TTL of DNS records (currently, a value of 1800 seconds is hardcoded)
+- remove old entries from the cache (currently, it's trivial for an attacker to make the cache grow indefinitely)
+- add support for "does not exist" replies (see https://tools.ietf.org/html/rfc6762#section-6.1)
---
+++ app.js
@@ -0,0 +1,61 @@
+﻿var dns = require('dns');
+var mdns = require('multicast-dns')({ interface: undefined });
+
+// set the interface option to a network interface name or IP to use a specific network interface
+// query cache
+var cache = {};
+
+// hardcoded TTL value
+var ttl = 1800;
+
+// handle Multicast DNS queries
+mdns.on('query', function (query) {
+    var time = new Date().getTime() / 1000;
+
+    //console.log('time:', time);
+    //console.log('query:', query);
+    query.questions.forEach(function (q) {
+        switch (q.type) {
+            case "A":
+            case "AAAA":
+                break;
+            default:
+                return;
+        }
+
+        // look for the result in our cache
+        var key = q.name + " [" + q.type + "]";
+        var value = cache[key];
+        if (typeof value != 'undefined') {
+            if ((time - value.time) < ttl) {
+                if (value.response != null) {
+                    //console.log("cached response: ", key, value.response);
+                    mdns.response(value.response);
+                } else {
+                    //console.log("ignoring: ", key);
+                }
+                return;
+            }
+        }
+
+        // resolve the query using DNS
+        dns.resolve(q.name, q.type, function (err, addresses) {
+            if (err != null) {
+                // on error, don't respond but add entry to our cache
+                //console.log("error: ", key, err);
+                cache[key] = { time: time, response: null };
+            } else {
+                // respond with the first address found
+                var address = addresses[0];
+                var response = [{ name: q.name, type: q.type, ttl: ttl, data: address }];
+                console.log("response: ", key, response);
+                mdns.response(response);
+
+                // add entry to our cache
+                cache[key] = { time: time, response: null };
+            }
+        });
+    });
+});
+
+console.log("Listening for mDNS queries...");
